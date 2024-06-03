b8cc4a55ed7a74704258a7215b1b05a0f7f88d25
Sonakshi Jha <118923764+15sonakshi@users.noreply.github.com>
2024-06-03 12:19:56 +0530

    Add files via upload

---
+++ appData.js
@@ -0,0 +1,56 @@
+export const rowData = [
+    {    
+        id: 1,
+        title: 'Car',
+        price: 10,
+        company: 'Toyota',
+        info: "One of the best cars.",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 2,
+        title: 'Car',
+        price: 20,
+        company: "Honda",
+        info: "Honda is the best company.",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 3,
+        title: 'Car',
+        price: 30,
+        company: "Ford",
+        info: "Ford is the best company.",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 4,
+        title: 'Car',
+        price: 40,
+        company: "Porsche",
+        info: "Porsche is the best company.",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 5,
+        title: 'Bike',
+        price: 50,
+        company: "Yamaha",
+        info: "Yamaha is the best company.",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 6,
+        title: 'Book',
+        price: 60,
+        company: "Book Agencies",
+        info: "Book Agencies is the best company.",
+        inCart: false,
+        count: 1
+    }
+];
